15ccceeeab0212291611b8378e476e3824d8cba6
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2022-03-29 20:58:17 -0700

    Added color icons

--- src/components/Assistance/AssistanceScreen.js
+++ src/components/Assistance/AssistanceScreen.js
@@ -54,6 +54,10 @@ export default function AssistanceScreen() {
         }
     }, []);
 
+
+
+
+
     return (
         <Container className='animate__animated animate__fadeIn'>
             <Box className='as-content--search'>
@@ -78,6 +82,35 @@ export default function AssistanceScreen() {
     );
 }
 
+function stringAvatar(name) {
+    return {
+        sx: {
+            bgcolor: stringToColor(name),
+        },
+        children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+    };
+}
+
+function stringToColor(string) {
+    let hash = 0;
+    let i;
+
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+        hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = '#';
+
+    for (i = 0; i < 3; i += 1) {
+        const value = (hash >> (i * 8)) & 0xff;
+        color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+
+    return color;
+}
+
 export const Person = ({ name, checkInDate, dependenciaName, leftAt }) => {
     const date = new Date(checkInDate);
     const dateLeft = new Date(leftAt);
@@ -85,8 +118,8 @@ export const Person = ({ name, checkInDate, dependenciaName, leftAt }) => {
         <>
             <ListItem alignItems="flex-start" className='as-listItem'>
                 <ListItemAvatar>
-                    <Avatar>
-                        <PersonIcon />
+                    <Avatar {...stringAvatar(name)}>
+
                     </Avatar>
                 </ListItemAvatar>
                 <ListItemText
